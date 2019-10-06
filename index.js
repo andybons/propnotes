79eb6883ce5d7db386b884cfce56c81007c37f39
Andrew Bonventre <andybons@gmail.com>
2019-10-06 17:15:50 -0400

    rewrite to use hooks 🎣

--- index.js
+++ index.js
@@ -1,170 +1,223 @@
 import './style';
 import { ISSUE_DATA } from './data.js';
-import { Component } from 'preact';
+import { useState } from 'preact/hooks';
 
-const members = ['@andybons', '@bradfitz', '@ianlancetaylor', '@rsc', '@spf13', '@griesemer'];
+const AttendeeList = ({ attendees, addAttendee, removeAttendee }) => {
+	const members = ['@andybons', '@bradfitz', '@ianlancetaylor', '@rsc', '@spf13', '@griesemer'];
+	return (
+		<>
+			<h2>Attendees</h2>
+			<ul className="AttendeeList">
+				{members.map(member => {
+					return (
+						<li className="Attendee" key={member}>
+							<label>
+								<input
+									className="Attendee-checkbox"
+									type="checkbox"
+									value={member}
+									onClick={e => {
+										e.target.checked ? addAttendee(member) : removeAttendee(member);
+									}}
+									checked={attendees.has(member)}
+								/>
+								{member}
+							</label>
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
+};
 
-export default class App extends Component {
-	state = {
-		attendees: new Set(),
-		filter: '',
-		selectedIssues: new Set(),
-		notes: {}, // issue number -> notes
-	};
+const Issue = ({ issue, selected, note, onClick, updateNote }) => (
+	<li className="Issue">
+		<label className="Issue-label">
+			<input
+				className="Issue-checkbox"
+				type="checkbox"
+				value={issue.number}
+				checked={selected}
+				onClick={e => {
+					onClick(e.target.checked);
+				}}
+			/>
+			<a href={'https://golang.org/issue/' + issue.number} target="_blank" rel="noopener">
+				{issue.number}
+			</a>{' '}
+			{issue.title}
+		</label>
+		<div hidden={!selected}>
+			<textarea
+				className="Issue-notes"
+				value={note}
+				onInput={e => {
+					updateNote(issue, e.target.value);
+				}}
+			/>
+		</div>
+	</li>
+);
 
-	handleAttendeeClick = e => {
-		const { checked, value } = e.target;
-		this.setState(({ attendees }) => {
-			if (checked) {
-				attendees.add(value);
-			} else {
-				attendees.delete(value);
+const IssueList = ({
+	issues,
+	selectedIssues,
+	addSelectedIssue,
+	removeSelectedIssue,
+	notes,
+	updateNote,
+}) => {
+	const [filter, setFilter] = useState('');
+	const [filteredIssues, setFilteredIssues] = useState([...issues]);
+
+	const filterIssues = filter => {
+		setFilter(filter);
+		const filteredIssues = issues.filter(issue => {
+			if (filter.trim() === '') {
+				return true;
 			}
-			return {
-				attendees: attendees,
-			};
+			filter = filter.toLowerCase();
+			return (
+				issue.title.toLowerCase().includes(filter) ||
+				(issue.number + '').toLowerCase().includes(filter)
+			);
 		});
+		setFilteredIssues(filteredIssues);
 	};
 
-	handleIssueClick = e => {
-		let { checked, value } = e.target;
-		value = +value; // coerce into number
-		this.setState(({ selectedIssues }) => {
-			if (checked) {
-				selectedIssues.add(value);
-			} else {
-				selectedIssues.delete(value);
-			}
-			return {
-				selectedIssues: selectedIssues,
-			};
-		});
+	return (
+		<>
+			<h2>Issues</h2>
+			<label className="Filter">
+				<span class="Filter-label">Filter</span>
+				<input
+					className="Filter-input"
+					type="text"
+					value={filter}
+					onInput={e => {
+						filterIssues(e.target.value);
+					}}
+				/>
+			</label>
+			<ul className="IssueList">
+				{filteredIssues.map(issue => {
+					return (
+						<Issue
+							key={issue.number}
+							issue={issue}
+							selected={selectedIssues.has(issue.number)}
+							onClick={selected => {
+								selected ? addSelectedIssue(issue) : removeSelectedIssue(issue);
+							}}
+							note={notes[issue.number]}
+							updateNote={updateNote}
+						></Issue>
+					);
+				})}
+			</ul>
+		</>
+	);
+};
+
+const MinutesOutput = ({ attendees, issues, selectedIssues, notes }) => {
+	return (
+		<>
+			<h2>Minutes</h2>
+			<pre className="Minutes">
+				**
+				{new Date().toISOString().substr(0, 10)} /{' '}
+				{Array.from(attendees)
+					.sort()
+					.join(', ')}
+				**
+				{'\n\n'}
+				{issues
+					.filter(issue => {
+						return selectedIssues.has(issue.number);
+					})
+					.map(issue => {
+						return { ...issue, ['title']: issue.title.replace(/proposal: /i, '') };
+					})
+					.sort((i1, i2) => i1.title.localeCompare(i2.title))
+					.map(issue => {
+						return (
+							`- #${issue.number} **${issue.title.replace('*', '\\*')}**\n` +
+							(notes[issue.number] || '')
+								.split('\n')
+								.filter(line => line.trim() !== '')
+								.map(line => `  - ${line}`)
+								.join('\n') +
+							'\n'
+						);
+					})}
+			</pre>
+		</>
+	);
+};
+
+export default function App() {
+	const [attendees, setAttendees] = useState(new Set());
+
+	const addAttendee = attendee => {
+		attendees.add(attendee);
+		setAttendees(new Set(attendees));
 	};
 
-	handleFilterInput = e => {
-		this.setState(prevState => {
-			return {
-				filter: e.target.value.toLowerCase(),
-			};
-		});
+	const removeAttendee = attendee => {
+		attendees.delete(attendee);
+		setAttendees(new Set(attendees));
 	};
 
-	handleTextareaInput = (issueNum, e) => {
-		this.setState(({ notes }) => {
-			notes[issueNum] = e.target.value;
-			return {
-				notes: notes,
-			};
-		});
+	const [selectedIssues, setSelectedIssues] = useState(new Set());
+
+	const addSelectedIssue = issue => {
+		selectedIssues.add(issue.number);
+		setSelectedIssues(new Set(selectedIssues));
 	};
 
-	render({}, { attendees, filter, selectedIssues, notes }) {
-		return (
-			<app>
-				<header>
-					<h1>Go Proposal Minutes Generator</h1>
-				</header>
-				<main>
-					<div class="u-column">
-						<h2>Input</h2>
-						<h3>Attendees</h3>
-						<ul>
-							{members.map(m => {
-								return (
-									<li key={m}>
-										<label>
-											<input type="checkbox" value={m} onClick={this.handleAttendeeClick} />
-											{m}
-										</label>
-									</li>
-								);
-							})}
-						</ul>
-						<h3>Issues</h3>
-						<label>
-							Filter: <input type="text" value={filter} onInput={this.handleFilterInput} />
-						</label>
-						<ul class="IssueList">
-							{ISSUE_DATA.filter(issue => {
-								if (filter.trim() === '') {
-									return true;
-								}
-								return (
-									issue.title.toLowerCase().includes(filter) ||
-									(issue.number + '').toLowerCase().includes(filter)
-								);
-							}).map(issue => {
-								return (
-									<li class="IssueList-item" key={issue.number}>
-										<label class="IssueList-itemLabel">
-											<input
-												type="checkbox"
-												value={issue.number}
-												checked={selectedIssues.has(issue.number)}
-												onClick={this.handleIssueClick}
-											/>
-											<a
-												href={'https://golang.org/issue/' + issue.number}
-												target="_blank"
-												rel="noopener"
-											>
-												{issue.number}
-											</a>{' '}
-											{issue.title}
-										</label>
-										<div hidden={!selectedIssues.has(issue.number)}>
-											<textarea
-												class="Issue-notes"
-												value={notes[issue.number]}
-												onInput={this.handleTextareaInput.bind(this, issue.number)}
-											/>
-										</div>
-									</li>
-								);
-							})}
-						</ul>
-					</div>
-					<aside class="u-column">
-						<h2>Output</h2>
-						<pre>
-							**
-							{new Date().toISOString().substr(0, 10)} /{' '}
-							{Array.from(attendees)
-								.sort()
-								.join(', ')}
-							**
-							{'\n\n'}
-							{ISSUE_DATA.filter(issue => {
-								return selectedIssues.has(issue.number);
-							})
-								.map(issue => {
-									issue.title = issue.title.replace(/proposal: /i, '');
-									return issue;
-								})
-								.sort((i1, i2) => {
-									if (i1.title < i2.title) {
-										return -1;
-									}
-									if (i1.title > i2.title) {
-										return 1;
-									}
-									return 0;
-								})
-								.map(issue => {
-									return (
-										`- #${issue.number} **${issue.title.replace('*', '\\*')}**\n` +
-										(notes[issue.number] || '')
-											.split('\n')
-											.filter(line => line.trim() != '')
-											.map(line => `  - ${line}`)
-											.join('\n') +
-										'\n'
-									);
-								})}
-						</pre>
-					</aside>
-				</main>
-			</app>
-		);
-	}
+	const removeSelectedIssue = issue => {
+		selectedIssues.delete(issue.number);
+		setSelectedIssues(new Set(selectedIssues));
+	};
+
+	const [notes, updateNotes] = useState({}); // issue number -> notes
+
+	return (
+		<>
+			<header className="Header">
+				<h1 className="Header-text">Go Proposal Minutes Generator</h1>
+			</header>
+			<main>
+				<div className="App-input">
+					<AttendeeList
+						attendees={attendees}
+						addAttendee={addAttendee}
+						removeAttendee={removeAttendee}
+					></AttendeeList>
+					<IssueList
+						issues={ISSUE_DATA}
+						selectedIssues={selectedIssues}
+						addSelectedIssue={addSelectedIssue}
+						removeSelectedIssue={removeSelectedIssue}
+						notes={notes}
+						updateNote={(issue, note) => {
+							updateNotes({
+								...notes,
+								[issue.number]: note,
+							});
+						}}
+					></IssueList>
+				</div>
+				<div className="App-output">
+					<MinutesOutput
+						attendees={attendees}
+						issues={ISSUE_DATA}
+						selectedIssues={selectedIssues}
+						notes={notes}
+					></MinutesOutput>
+				</div>
+			</main>
+		</>
+	);
 }
